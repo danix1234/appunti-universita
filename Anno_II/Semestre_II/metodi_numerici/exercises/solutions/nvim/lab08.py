0e922d0125499ede39644c8f2b3a813ae4ff41ce
Daniele Muffato <danixgithub1@gmail.com>
2024-04-30 09:32:15 +0200

    wip metodi numerici exercise

--- Anno_II/Semestre_II/metodi_numerici/exercises/solutions/nvim/lab08.py
+++ Anno_II/Semestre_II/metodi_numerici/exercises/solutions/nvim/lab08.py
@@ -1,1 +1,34 @@
 #!/bin/env python3
+
+import numpy as np
+
+
+def steepest_descent(A, b, x0, itmax, toll):
+    n, m = A.shape
+    if n != m:
+        print("Matrice non quadrata")
+        return None, None, None, None
+
+    x = x0
+    r = A@x - b
+    p = -r
+    it = 0
+    nb = np.linalg.norm(b)
+    criterio_di_arresto = np.linalg.norm(r) / nb
+    vec_sol = [x]
+    vec_r = [criterio_di_arresto]
+
+    while criterio_di_arresto >= toll and it < itmax:
+        it += 1
+        Ap = A@p
+        num = -(r.T@p)
+        alfa = num / (p.T@Ap)
+
+        x = x + alfa*p
+        r = r+alfa*Ap
+        vec_sol.append(x)
+        criterio_di_arresto = np.linalg.norm(r) / nb
+        vec_r.append(criterio_di_arresto)
+        p = -r
+
+    return x, vec_r, vec_sol, it
